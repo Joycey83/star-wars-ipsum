b74616899761584b4e80f6db1cb6317709e3d02c
Joyce Cheung <Jcheung8385@gmail.com>
2024-04-15 17:33:36 +0100

    added the basic structure of the form, imported usestate

--- src/components/LoremText.jsx
+++ src/components/LoremText.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Title from "./Title";
 
 const LoremText = () => {
+  const [countNum, setCountNum] = useState(1);
   return (
     <main>
       <Title />
+
+      <form>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          value={countNum}
+          id="amount"
+          step="1"
+          min="1"
+          max="8"
+        />
+        <button type="submit">Generate</button>
+      </form>
     </main>
   );
 };
