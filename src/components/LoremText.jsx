b47c6fd05578ea1ebe49ae74f47a2cfef21cbd46
Joyce Cheung <Jcheung8385@gmail.com>
2024-04-15 18:50:35 +0100

    added a paragraph to my useState value so it is not an empty array

--- src/components/LoremText.jsx
+++ src/components/LoremText.jsx
@@ -5,7 +5,9 @@ import Title from "./Title";
 
 const LoremText = () => {
   const [countNum, setCountNum] = useState(1);
-  const [paragraph, setParagraph] = useState([]);
+  const [paragraph, setParagraph] = useState([
+    "LUKE: Hurry up! Come with me! What are you waiting for?! Get in gear! The robot scoots around in a tight circle, stops short, and smoke begins to pour out of every joint. Luke throws his arms up in disgust. Exasperated, the young farm boy jumps into his Landspeeder leavingthesmoldering robot to hum madly.",
+  ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
