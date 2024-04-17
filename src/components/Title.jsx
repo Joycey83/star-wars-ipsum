e8d33ce5c3519c4ee03c40367947a56fac079e6d
Joyce Cheung <Jcheung8385@gmail.com>
2024-04-17 14:42:13 +0100

    added more styles to the input, label and added a h1 to the title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,9 +2,12 @@ import titleLogo from "../images/star-wars-logo.png";
 
 const Title = () => {
   return (
-    <div className="title-img-container">
-      <img src={titleLogo} alt="Star wars logo" className="logo" />
-    </div>
+    <>
+      <div className="title-img-container">
+        <img src={titleLogo} alt="Star wars logo" className="logo" />
+      </div>
+      <h1 className="main-title">may the Ipsum be with you</h1>
+    </>
   );
 };
 export default Title;
